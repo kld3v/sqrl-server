059bbe56cc38d2d97030156825544b2134dc8c4f
MajidNeo <majidrezaeophy1879@gmail.com>
2024-01-23 08:53:10 +0000

    Scoring system changed to 1000

--- app/Http/Controllers/RiskEvaluationController.php
+++ app/Http/Controllers/RiskEvaluationController.php
@@ -19,46 +19,44 @@ class RiskEvaluationController extends Controller
 
     public function checkWebRisk(Request $request)
     {
-        $uri = 'http://49.89.186.145:52736/.i';
+        $uri = 'www.google.com';
         $apiKeyWebRisk = env('WEB_RISK_API_KEY');
 
         // Check with Google Web Risk service
         $googleWebRiskResult = $this->webRiskService->checkWebRisk($uri, $apiKeyWebRisk);
         $threatTypesToCheck = ['UNWANTED_SOFTWARE', 'MALWARE', 'SOCIAL_ENGINEERING', 'SOCIAL_ENGINEERING_EXTENDED_COVERAGE'];
 
+        $threatDetected = false;
+
         foreach ($threatTypesToCheck as $threatType) {
             if (
                 isset($googleWebRiskResult[$threatType]['body']['threat']['threatTypes'])
                 && in_array($threatType, $googleWebRiskResult[$threatType]['body']['threat']['threatTypes'])
             ) {
-                // If a threat is detected by Google Web Risk, return 'security_score' as 0
+                $threatDetected = true;
+                break;
+            }
+        }
+
+        if (!$threatDetected) {
+            // If no threat is detected by Google Web Risk, use VirusTotalService for additional check
+            $virusTotalResult = $this->virusTotalService->scanUrl($uri);
+            $analysisId = $virusTotalResult['data']['id'];
+            $analysisDetails = $this->virusTotalService->getAnalysisDetails($analysisId);
+
+            if ($analysisDetails['data']['attributes']['stats']['malicious'] > 0) {
                 return response()->json([
                     'security_score' => 0,
-                    'virus_total_malicious_count' => 0,  // No need to check VirusTotal if Google Web Risk detected a threat
+                    'virus_total_malicious_count' => $analysisDetails['data']['attributes']['stats']['malicious'],
+                    //'virus_total_result' => $virusTotalResult,
                 ]);
             }
         }
 
-        // If no threat is detected by Google Web Risk, use VirusTotalService for additional check
-        $virusTotalResult = $this->virusTotalService->scanUrl($uri);
-        $analysisId = $virusTotalResult['data']['id'];
-        $analysisDetails = $this->virusTotalService->getAnalysisDetails($analysisId);
-        //return response()->json($analysisDetails['data']['attributes']['stats']['malicious']); 
-        // Check if the 'malicious' count is greater than 0
-        
-
-        if ($analysisDetails['data']['attributes']['stats']['malicious']>0) {
-            return response()->json([
-                'security_score' => 0,
-                'virus_total_malicious_count' => $analysisDetails['data']['attributes']['stats']['malicious'],
-                //'virus_total_result' => $virusTotalResult,
-            ]);
-        }
-
         // If neither Google Web Risk nor VirusTotal indicates a threat, return 'security_score' as 500
         return response()->json([
-            'security_score' => 500,
-            'virus_total_malicious_count' => 0,  // No malicious detections from VirusTotal
+            'security_score' => 1000,
+            'virus_total_malicious_count' => 0
         ]);
     }
 }
