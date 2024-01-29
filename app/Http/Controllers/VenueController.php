e33eb952644fa6b98ba7be36d8581990484cb414
workjoelharris <work@joelharris.co.uk>
2024-01-29 13:44:09 +0000

    nearby not quite working.

--- app/Http/Controllers/VenueController.php
+++ app/Http/Controllers/VenueController.php
@@ -32,23 +32,23 @@ class VenueController extends Controller
         $request->validate([
             'latitude' => 'required|numeric',
             'longitude' => 'required|numeric',
+            'accuracy' => 'nullable|numeric'
         ]);
 
         $latitude = $request->input('latitude');
         $longitude = $request->input('longitude');
-
-        $distance = 1000;
+        $accuracy = $request->input('accuracy', 1000);
 
         // Query to find nearby venues within the specified distance
-        $venues = Venue::with('url') // Eager load the related URL data
+        $venues = Venue::with('url')
             ->select('id', 'company', 'chain', 'url_id', 'tel', 'address', 'postcode', 'google_maps')
-            ->whereRaw("ST_Distance_Sphere(
-                            point(longitude, latitude), 
-                            point(?, ?)
-                        ) <= ?", [$longitude, $latitude, $distance])
+            ->whereRaw("ST_Distance(
+                            area, 
+                            ST_GeomFromText(CONCAT('POINT(', ?, ' ', ?, ')'))
+                        ) <= ?", [$longitude, $latitude, $accuracy])
             ->get();
+    
 
-        // Return the results as JSON
         return response()->json($venues);
     }
 }
--- database/seeders/VenueSeeder.php
+++ database/seeders/VenueSeeder.php
@@ -17,22 +17,25 @@ class VenueSeeder extends Seeder
      */
     public function run()
     {
-        // First, create or find the URL record
-        $url = URL::firstOrCreate(['url' => 'https://alacrityfoundation.co.uk/']);
+        $alacrityUrl = URL::firstOrCreate(['url' => 'https://alacrityfoundation.co.uk/', 'trust_score' => 0]);
 
-        // Now, create the Venue record
         Venue::create([
             'company' => 'Alacrity',
             'chain' => 'Newport',
-            'url_id' => $url->id,
+            'url_id' => $alacrityUrl->id,
             'area' => DB::raw("ST_PolygonFromText('POLYGON((51.5877159 -2.9931069, 51.5878166 -2.9928685, 51.5876624 -2.9926144, 51.5875762 -2.9928780, 51.5877159 -2.9931069))')")
         ]);
 
+        $nandosUrl = URL::firstOrCreate(['url' => 'https://www.nandos.co.uk/eat-in-landing/newport-friars-walk', 'trust_score' => 100]);
+
         Venue::create([
-            'company' => 'Alacrity',
-            'chain' => 'Newport',
-            'url_id' => $url->id,
-            'area' => DB::raw("ST_PolygonFromText('POLYGON((51.5877159 -2.9931069, 51.5878166 -2.9928685, 51.5876624 -2.9926144, 51.5875762 -2.9928780, 51.5877159 -2.9931069))')")
+            'company' => 'Nando\'s',
+            'chain' => 'Newport - Friars Walk',
+            'url_id' => $nandosUrl->id,
+            'tel' => '01633215205',
+            'address' => '73 Usk Plaza Unit R4, Friars Walk Shopping Centre NP20 1DS',
+            'postcode' => 'NP20 1DS',
+            'area' => DB::raw("ST_PolygonFromText('POLYGON((51.586312850599846 -2.9922399197930285, 51.58607526800188 -2.9920521795881667, 51.58596769316683 -2.99259691005394, 51.58616280037874 -2.9927551818310656, 51.586312850599846 -2.9922399197930285))')")
         ]);
     }
 }
