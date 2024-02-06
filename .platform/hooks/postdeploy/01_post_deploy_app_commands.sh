3cdcb0cc1cfb4ae99b2eb46639bc245ba175a08f
Nathan Woodhouse <nathan@calibraelearning.com>
2024-02-06 16:53:40 +0000

    Added ebextensions and AWS nginx config files.

---
+++ .platform/hooks/postdeploy/01_post_deploy_app_commands.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+php artisan route:cache
+php artisan config:cache
+php artisan view:cache
+php artisan event:cache
+
+# Ensure that the newly created cache files can all be accessed properly
+chown -R webapp:webapp storage
+chmod -R 776 storage
