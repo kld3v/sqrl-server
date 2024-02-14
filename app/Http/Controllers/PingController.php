319ee99f67def5edaf685b6134c2c7bb26f5e1fd
workjoelharris <work@joelharris.co.uk>
2024-02-14 11:01:40 +0000

    test

--- app/Http/Controllers/PingController.php
+++ app/Http/Controllers/PingController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ScanProcessingService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class PingController extends Controller
 {
     public function ping() {
+
+        app(ScanProcessingService::class)->processRequest('http://example.com');
         return [
             'time' => now(),
             'auth' => Auth::user(),
