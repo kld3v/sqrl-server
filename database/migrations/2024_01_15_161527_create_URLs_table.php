e08828281022cf0d2bea7f48564bf362b7e61462
workjoelharris <work@joelharris.co.uk>
2024-02-19 13:43:40 +0000

    Scan Table Entry Fix

--- database/migrations/2024_01_15_161527_create_URLs_table.php
+++ database/migrations/2024_01_15_161527_create_URLs_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('urls', function (Blueprint $table) {
             $table->id();
-            $table->string('url', 2048); //UNIQUE ISSUE
+            $table->string('url', 2048)->unique();
             $table->smallInteger('trust_score');
             $table->timestamps();
         });
--- database/migrations/2024_01_15_161530_create_scans_table.php
+++ database/migrations/2024_01_15_161530_create_scans_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->unsignedBigInteger('url_id');
             $table->foreign('url_id')->references('id')->on('urls');
             $table->smallInteger('trust_score');
-            $table->char('device_uuid', 36)->unique();
+            $table->char('device_uuid', 36);
             $table->decimal('latitude', 9, 6)->nullable();
             $table->decimal('longitude', 9, 6)->nullable();
             $table->timestamps();
