b52bcb9ce049b6f3d1b2c87a3835ce52427bef5a
workjoelharris <work@joelharris.co.uk>
2024-02-28 16:49:44 +0000

    Setup

---
+++ resources/js/Pages/Joel.vue
@@ -0,0 +1,52 @@
+<script>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+// Options API
+export default {
+
+    components: {
+        AuthenticatedLayout,
+        Head
+    },
+
+    methods: {
+        doClick() {
+            alert('clicked');
+
+        }
+    },
+
+    props: {
+        message: {
+            required: true,
+            type: String,
+        }
+    }
+}
+
+</script>
+
+<template>
+    <Head title="This is the title in the browser tab" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Joel is the man!</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        You are here!
+                    </div>
+
+                    <h3>This is a heading: {{ message }}</h3>
+
+                    <button @click="doClick">Click me!</button>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Services\EvaluateTrustService;
-use App\Http\Controllers\URLmainController;
-use App\Http\Controllers\RiskEvaluationController;
-use App\Http\Controllers\TestEvaluateTrustController;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,14 +11,26 @@ use App\Http\Controllers\TestEvaluateTrustController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-   return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
+
+Route::get('/joel', function () {
+    return Inertia::render('Joel', [
+        'message' => "message",
+    ]);
+})->name('joel-route-name');
+
 Route::get('/check-web-risk', [EvaluateTrustService::class, 'evaluateTrust']);
 Route::get('/test-evaluate-trust', function () {
    $url = 'http://182.127.71.143:33800/Mozi.m';
@@ -28,3 +39,14 @@ Route::get('/test-evaluate-trust', function () {
    return response()->json($result);
 });
 
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
