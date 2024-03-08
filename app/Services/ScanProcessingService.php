dc92094b27594521e96b2af092c9917f076b2ce6
workjoelharris <work@joelharris.co.uk>
2024-03-08 14:09:20 +0000

    should be fixed

--- app/Services/ScanProcessingService.php
+++ app/Services/ScanProcessingService.php
@@ -36,7 +36,6 @@ class ScanProcessingService
         $redirectionValue = new RedirectionValue();
         $headlessBrowser = new HeadlessBrowser();
         if ($redirectionValue->redirectionValue($url)) {
-            var_dump('true');
             // var_dump('entered url' . $url);
             //$url = $this->shortURLMain->unshorten($url);
             $url = $headlessBrowser->interactWithPage($url);
--- app/Services/UrlManipulations/RedirectionValue.php
+++ app/Services/UrlManipulations/RedirectionValue.php
@@ -6,7 +6,7 @@ class RedirectionValue
 {
     public function redirectionValue($url)
     {
-        Log::channel('redirectLog')->debug("Checking redirection status for URL: {$url}");
+        // Log::channel('redirectLog')->debug("Checking redirection status for URL: {$url}");
 
         $curl = curl_init($url);
 
@@ -19,14 +19,14 @@ class RedirectionValue
 
         curl_close($curl);
 
-        Log::channel('redirectLog')->debug("HTTP status code for URL {$url}: {$httpStatusCode}");
+        // Log::channel('redirectLog')->debug("HTTP status code for URL {$url}: {$httpStatusCode}");
 
         // var_dump('curl status is' . $httpStatusCode);
         if ($httpStatusCode >= 300 && $httpStatusCode < 400) {
-            Log::channel('redirectLog')->info("URL {$url} is a redirect.");
+            // Log::channel('redirectLog')->info("URL {$url} is a redirect.");
            return true;
         } else {
-            Log::channel('redirectLog')->info("URL {$url} is not a redirect.");
+            // Log::channel('redirectLog')->info("URL {$url} is not a redirect.");
             return false;
         }
     }
