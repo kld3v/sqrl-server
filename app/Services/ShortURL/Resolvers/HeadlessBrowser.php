a44148193ad63904bf0d0ece6a49c608cc638bfb
MajidNeo <majidrezaeophy1879@gmail.com>
2024-03-20 13:12:52 +0000

    closing the headless b after each iteration

--- app/Services/ShortURL/Resolvers/HeadlessBrowser.php
+++ app/Services/ShortURL/Resolvers/HeadlessBrowser.php
@@ -28,7 +28,7 @@ class HeadlessBrowser
                 $lastUrl = $page->getCurrentUrl();
 
                 Log::channel('redirectLog')->info("Navigation completed. Final URL");
-
+                $browser->close();
                 return $lastUrl;
             } catch (BrowserConnectionFailed $e) {
                 Log::channel('redirectLog')->error("Browser connection failed during navigation: " . $e->getMessage());
