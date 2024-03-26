71a4d61e886054bf6d1c2408b6b53d62b0f9639c
workjoelharris <work@joelharris.co.uk>
2024-03-26 09:48:43 +0000

    Add basic middleware to test scan api

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        'check.basic.phrase' => \App\Http\Middleware\CheckBasicPhraseInHeader::class,
     ];
 }
