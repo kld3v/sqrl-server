6c5a371835277ef52be532f32d0349df62622bd9
joelaharris98 <joelaharris98@gmail.com>
2024-04-10 08:31:17 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AppleAuthController;
+use App\Http\Controllers\Auth\GoogleAuthController;
 
 /*
 |--------------------------------------------------------------------------
