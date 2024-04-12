33c29dd75499828bc9c75c35cbdab968283956f8
joelaharris98 <joelaharris98@gmail.com>
2024-04-12 14:38:12 +0100

    Update FakeLeaderboardController.php

--- app/Http/Controllers/FakeLeaderboardController.php
+++ app/Http/Controllers/FakeLeaderboardController.php
@@ -10,7 +10,7 @@ class FakeLeaderboardController extends Controller
     public function index()
     {
         $leaderboardService = new FakeLeaderboards();
-        $data = $leaderboardService->generateLeaderboard(19); 
+        $data = $leaderboardService->generateLeaderboard(9); 
 
         return response()->json($data);
     }
